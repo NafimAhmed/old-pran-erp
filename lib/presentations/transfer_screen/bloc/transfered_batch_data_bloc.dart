46d6e4bbd30817de0913c1f802c77b83adf52f77
miraj98hossain <miraj09hossain@gmail.com>
2024-10-17 11:33:46 +0600

    transfered batch data get bloc created

---
+++ lib/presentations/transfer_screen/bloc/transfered_batch_data_bloc.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pran_rfl_erp/app_data/entities/transfer_batch_data_response.dart';
+import 'package:pran_rfl_erp/app_data/service/data_service.dart';
+
+@immutable
+sealed class TransferedBatchDataEvent {}
+
+final class TransferBatchDataGet extends TransferedBatchDataEvent {}
+
+@immutable
+sealed class TransferedBatchDataState {}
+
+final class TransferedBatchDataInitial extends TransferedBatchDataState {}
+
+final class TransferedBatchDataLoading extends TransferedBatchDataState {}
+
+final class TransferedBatchDataSuccess extends TransferedBatchDataState {
+  final List<TransferBatchData> transferBatchDataList;
+
+  TransferedBatchDataSuccess({required this.transferBatchDataList});
+}
+
+final class TransferedBatchDataError extends TransferedBatchDataState {
+  final Object error;
+
+  TransferedBatchDataError({required this.error});
+}
+
+class TransferedBatchDataBloc
+    extends Bloc<TransferedBatchDataEvent, TransferedBatchDataState> {
+  final DataService _dataService;
+  TransferedBatchDataBloc(this._dataService)
+      : super(TransferedBatchDataInitial()) {
+    on<TransferBatchDataGet>((event, emit) async {
+      emit(TransferedBatchDataLoading());
+      try {
+        var response = await _dataService.getTransferBatchData();
+        emit(TransferedBatchDataSuccess(transferBatchDataList: response));
+      } catch (error) {
+        emit(TransferedBatchDataError(error: error));
+      }
+    });
+  }
+}
