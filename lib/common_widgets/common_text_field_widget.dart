1942611f7cbc9fb501050ab78afcd8702b95c5d6
miraj98hossain <miraj09hossain@gmail.com>
2024-11-09 15:20:30 +0600

    default padding added

--- lib/common_widgets/common_text_field_widget.dart
+++ lib/common_widgets/common_text_field_widget.dart
@@ -46,7 +46,6 @@ class CommonTextFieldWidget extends StatelessWidget {
           ),
       decoration: InputDecoration(
         labelText: labelText,
-        contentPadding: EdgeInsets.zero,
       ).applyDefaults(
         Theme.of(context).inputDecorationTheme,
       ),
