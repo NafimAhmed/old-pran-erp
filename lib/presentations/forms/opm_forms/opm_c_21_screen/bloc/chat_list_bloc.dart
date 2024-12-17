bb503e37e458b970106b19e94c5629f2bfe30be5
miraj98hossain <miraj09hossain@gmail.com>
2024-12-17 13:59:47 +0600

    chat list bloc created and integrated

---
+++ lib/presentations/forms/opm_forms/opm_c_21_screen/bloc/chat_list_bloc.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pran_rfl_erp/app_data/models/chat_list_response.dart';
+import 'package:pran_rfl_erp/app_data/service/data_service.dart';
+
+@immutable
+sealed class ChatListEvent {}
+
+final class GetConversation extends ChatListEvent {
+  final String userId;
+
+  GetConversation({
+    required this.userId,
+  });
+}
+
+@immutable
+sealed class ChatListState {}
+
+final class ChatInitial extends ChatListState {}
+
+final class ChatListLoading extends ChatListState {}
+
+final class ChatListSuccess extends ChatListState {
+  final List<GptInfo> conversation;
+
+  ChatListSuccess({required this.conversation});
+}
+
+final class ChatListError extends ChatListState {
+  final Object error;
+
+  ChatListError({required this.error});
+}
+
+class ChatListBloc extends Bloc<ChatListEvent, ChatListState> {
+  final DataService _dataService;
+  ChatListBloc(this._dataService) : super(ChatInitial()) {
+    on<GetConversation>((event, emit) async {
+      emit(ChatListLoading());
+      try {
+        var response = await _dataService.getMessages(
+          userid: event.userId,
+        );
+        emit(ChatListSuccess(conversation: response));
+      } catch (e) {
+        emit(ChatListError(error: e));
+      }
+    });
+  }
+}
