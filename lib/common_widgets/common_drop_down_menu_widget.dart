74ea1d1782e44324381d86e159fa0d640f737ac6
miraj98hossain <miraj09hossain@gmail.com>
2025-01-01 14:33:11 +0600

    dropdown menu height modifed

--- lib/common_widgets/common_drop_down_menu_widget.dart
+++ lib/common_widgets/common_drop_down_menu_widget.dart
@@ -18,70 +18,75 @@ class CommonDropDownMenuWidget<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DropdownMenu<T>(
-      menuHeight: 250,
-      expandedInsets: EdgeInsets.zero,
-      enableSearch: false,
-      requestFocusOnTap: true,
-      enabled: enabled,
-      enableFilter: true,
-      controller: controller,
-      hintText: hintText,
-      filterCallback: (entries, filter) {
-        try {
-          final String trimmedFilter = filter.trim().toLowerCase();
-          if (trimmedFilter.isEmpty) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.060,
+      child: DropdownMenu<T>(
+        menuHeight: 250,
+        expandedInsets: EdgeInsets.zero,
+        enableSearch: false,
+        requestFocusOnTap: true,
+        enabled: enabled,
+        enableFilter: true,
+        controller: controller,
+        hintText: hintText,
+        filterCallback: (entries, filter) {
+          try {
+            final String trimmedFilter = filter.trim().toLowerCase();
+            if (trimmedFilter.isEmpty) {
+              return entries;
+            }
+            List<DropdownMenuEntry<T>> fentries = entries
+                .where(
+                  (DropdownMenuEntry<T> entry) =>
+                      entry.label.toLowerCase().contains(trimmedFilter),
+                )
+                .toList();
+            return fentries.isEmpty ? [] : fentries;
+          } catch (e) {
             return entries;
           }
-          List<DropdownMenuEntry<T>> fentries = entries
-              .where(
-                (DropdownMenuEntry<T> entry) =>
-                    entry.label.toLowerCase().contains(trimmedFilter),
-              )
-              .toList();
-          return fentries.isEmpty ? [] : fentries;
-        } catch (e) {
-          return entries;
-        }
-      },
-      inputDecorationTheme: InputDecorationTheme(
-        fillColor: appTheme.white,
-        filled: true,
-        hintStyle: textTheme.bodySmall!.copyWith(
+        },
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: appTheme.white,
+          filled: true,
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+          hintStyle: textTheme.bodySmall!.copyWith(
+            color: appTheme.primary,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        textStyle: textTheme.bodySmall!.copyWith(
           color: appTheme.primary,
           fontSize: 15,
           fontWeight: FontWeight.bold,
         ),
-      ),
-      textStyle: textTheme.bodySmall!.copyWith(
-        color: appTheme.primary,
-        fontSize: 15,
-        fontWeight: FontWeight.bold,
-      ),
-      onSelected: onSelected,
-      dropdownMenuEntries: List.generate(
-        dropdownMenuEntries.length,
-        (index) {
-          return DropdownMenuEntry(
-            style: ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll(
-                index % 2 == 0
-                    ? appTheme.primary.withOpacity(0.2)
-                    : appTheme.white,
+        onSelected: onSelected,
+        dropdownMenuEntries: List.generate(
+          dropdownMenuEntries.length,
+          (index) {
+            return DropdownMenuEntry(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(
+                  index % 2 == 0
+                      ? appTheme.primary.withOpacity(0.2)
+                      : appTheme.white,
+                ),
               ),
-            ),
-            value: dropdownMenuEntries[index],
-            label: dropdownMenuEntries[index].toString(),
-            labelWidget: Text(
-              dropdownMenuEntries[index].toString(),
-              style: textTheme.bodySmall!.copyWith(
-                color: appTheme.primary,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
+              value: dropdownMenuEntries[index],
+              label: dropdownMenuEntries[index].toString(),
+              labelWidget: Text(
+                dropdownMenuEntries[index].toString(),
+                style: textTheme.bodySmall!.copyWith(
+                  color: appTheme.primary,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
