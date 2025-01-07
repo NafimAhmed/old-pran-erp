4a882b82d400cb6fdb36687c47ed3a55a1ede023
miraj98hossain <miraj09hossain@gmail.com>
2025-01-07 17:06:16 +0600

    Task Status Change Screen Modifed added Dismissible Widget also List Widget Modified

--- lib/presentations/forms/om_forms/om_c_7_screen/om_c_7_screen.dart
+++ lib/presentations/forms/om_forms/om_c_7_screen/om_c_7_screen.dart
@@ -11,7 +11,6 @@ import 'package:pran_rfl_erp/core/extentions/extentions.dart';
 import 'package:pran_rfl_erp/core/theme/app_theme.dart';
 import 'package:pran_rfl_erp/global_blocs/cubit/logged_user_info_cubit.dart';
 import 'package:pran_rfl_erp/global_blocs/cubit/variable_state_handler_cubit.dart';
-import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/job_task_info_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/task_Save_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/task_info_bloc.dart';
 
@@ -25,20 +24,14 @@ class OmC7Screen extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => JobTaskInfoBloc(getService()),
-        ),
-        BlocProvider(
           create: (context) => TaskInfoBloc(getService()),
         ),
         BlocProvider(
           create: (context) => TaskSaveBloc(getService()),
         ),
         BlocProvider(
-          create: (context) => VariableStateHandlerCubit<TaskInfo>(),
+          create: (context) => VariableStateHandlerCubit<String>(),
         ),
-        BlocProvider(
-          create: (context) => VariableStateHandlerCubit<TaskStatusType>(),
-        )
       ],
       child: OmC7ScreenBody(
         fromName: fromName,
@@ -55,36 +48,34 @@ class OmC7ScreenBody extends StatefulWidget {
 }
 
 enum TaskStatusType {
-  start("START"),
-  closed("CLOSED"),
-  completed("COMPLETED"),
-  pending("PENDING");
+  start("Start"),
+  closed("Closed"),
+  completed("Completed"),
+  pending("Pending");
 
   const TaskStatusType(this.value);
 
   final String value;
   @override
   String toString() {
-    return name;
+    return value;
   }
 }
 
 class _OmC7ScreenBodyState extends State<OmC7ScreenBody> {
   TextEditingController taskTextEditingController = TextEditingController();
+  TextEditingController orgDropDownTextController = TextEditingController();
   late UserInfoModel loggedUser;
   @override
   void initState() {
     loggedUser = context.read<LoggedUserInfoCubit>().state!;
-    context.read<JobTaskInfoBloc>().add(
-          GetJobTaskInfo(
-            userId: loggedUser.userId,
-          ),
-        );
+
     context.read<TaskInfoBloc>().add(
           GetTaskInfo(
             userId: loggedUser.userId,
           ),
         );
+
     super.initState();
   }
 
@@ -101,9 +92,9 @@ class _OmC7ScreenBodyState extends State<OmC7ScreenBody> {
       body: BlocListener<TaskSaveBloc, TaskSaveState>(
         listener: (context, state) {
           if (state is TaskSaveSuccess) {
-            taskTextEditingController.clear();
-            context.read<VariableStateHandlerCubit<TaskInfo>>().reset();
-            context.read<VariableStateHandlerCubit<TaskStatusType>>().reset();
+            // taskTextEditingController.clear();
+
+            // context.read<VariableStateHandlerCubit<TaskStatusType>>().reset();
             ScaffoldMessenger.of(context).showSnackBar(
               CustomSnackBar.successSnackber(
                 message: "Successfully Saved",
@@ -132,79 +123,24 @@ class _OmC7ScreenBodyState extends State<OmC7ScreenBody> {
               const SizedBox(
                 height: 10,
               ),
-              BlocBuilder<JobTaskInfoBloc, JobTaskInfoState>(
-                builder: (context, state) {
-                  return CommonDropDownMenuWidget<TaskInfo>(
-                    controller: taskTextEditingController,
-                    enabled: state is JobTaskInfoSuccess
-                        ? state.jobTaskInfoList.isNotEmpty
-                        : false,
-                    hintText: "Select Task",
-                    dropdownMenuEntries: state is JobTaskInfoSuccess
-                        ? state.jobTaskInfoList
-                        : [],
-                    onSelected: (value) {
-                      if (value != null) {
-                        context
-                            .read<VariableStateHandlerCubit<TaskInfo>>()
-                            .update(value);
-                      }
-                    },
-                  );
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: CommonDropdownButton<TaskStatusType>(
-                      value: context
-                          .watch<VariableStateHandlerCubit<TaskStatusType>>()
-                          .state,
-                      onChanged: (value) {
-                        if (value != null) {
-                          context
-                              .read<VariableStateHandlerCubit<TaskStatusType>>()
-                              .update(value);
-                        }
-                      },
-                      hintText: "Task Status",
-                      items: TaskStatusType.values,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  BlocBuilder<TaskSaveBloc, TaskSaveState>(
-                    builder: (context, state) {
-                      return ElevatedButton(
-                        onPressed: () {
-                          var selectedTask = context
-                              .read<VariableStateHandlerCubit<TaskInfo>>()
-                              .state!;
-                          var selectedStatus = context
-                              .read<VariableStateHandlerCubit<TaskStatusType>>()
-                              .state!;
-                          context.read<TaskSaveBloc>().add(
-                                TaskSave(
-                                  userId: loggedUser.userId,
-                                  taskStatus: selectedStatus.value,
-                                  taskId: selectedTask.tasksid ?? 0,
-                                ),
-                              );
-                        },
-                        child: Text(
-                          state is TaskSaveLoading ? "Saving.." : "Save",
-                          style: textTheme.bodyMedium!.copyWith(
-                            color: appTheme.white,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
+              CommonDropDownMenuWidget<String>(
+                hintText: "Job Order No",
+                enabled: true,
+                controller: orgDropDownTextController,
+                dropdownMenuEntries: const [
+                  "DOLLAR TREE-14-25",
+                  "DOLLAR TREE-13-25",
+                  "DOLLAR TREE-12-25",
+                  "COLOR BABY-07-24",
+                  "BIRGMA-24-12"
                 ],
+                onSelected: (value) {
+                  if (value != null) {
+                    context
+                        .read<VariableStateHandlerCubit<String>>()
+                        .update(value);
+                  }
+                },
               ),
               const SizedBox(
                 height: 10,
@@ -221,86 +157,9 @@ class _OmC7ScreenBodyState extends State<OmC7ScreenBody> {
                       return ListView.separated(
                         itemBuilder: (context, index) {
                           var data = state.taskInfoList[index];
-                          return Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: appTheme.white,
-                              borderRadius: BorderRadius.circular(
-                                10,
-                              ),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data.taskName ?? "",
-                                  style: textTheme.bodyMedium!.copyWith(
-                                    color: appTheme.primary,
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Department",
-                                      style: textTheme.bodyMedium!.copyWith(
-                                        color: appTheme.primary,
-                                      ),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                        data.taskDept ?? "",
-                                        style: textTheme.bodyMedium!.copyWith(
-                                          color: appTheme.primary,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Status",
-                                      style: textTheme.bodyMedium!.copyWith(
-                                        color: appTheme.primary,
-                                      ),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                        data.taskStatus ?? "",
-                                        style: textTheme.bodyMedium!.copyWith(
-                                          color: appTheme.primary,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Start Date",
-                                      style: textTheme.bodyMedium!.copyWith(
-                                        color: appTheme.primary,
-                                      ),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                        DateTime.parse(data.taskStartDate ?? "")
-                                            .toFormatedString("dd-MMM-yyyy"),
-                                        style: textTheme.bodyMedium!.copyWith(
-                                          color: appTheme.primary,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
+                          return TaskWidget(
+                            index: index,
+                            data: data,
                           );
                         },
                         separatorBuilder: (context, index) => const SizedBox(
@@ -314,6 +173,212 @@ class _OmC7ScreenBodyState extends State<OmC7ScreenBody> {
                 ),
               )
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TaskWidget extends StatelessWidget {
+  const TaskWidget({
+    super.key,
+    required this.data,
+    required this.index,
+  });
+
+  final TaskInfo data;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => VariableStateHandlerCubit<TaskStatusType>(),
+      child: TaskWidgetContent(
+        data: data,
+        index: index,
+      ),
+    );
+  }
+}
+
+class TaskWidgetContent extends StatelessWidget {
+  const TaskWidgetContent({super.key, required this.data, required this.index});
+
+  final TaskInfo data;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Dismissible(
+        key: Key(data.tasksid.toString()),
+        direction:
+            context.watch<VariableStateHandlerCubit<TaskStatusType>>().state !=
+                    null
+                ? DismissDirection.startToEnd
+                : DismissDirection.none,
+        dismissThresholds: const {DismissDirection.startToEnd: 0.8},
+        confirmDismiss: (direction) async {
+          return await showDialog<bool>(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Are You Sure"),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text("Cancel"),
+                        onPressed: () {
+                          Navigator.pop(
+                              context, false); // Return false if cancelled
+                        },
+                      ),
+                      TextButton(
+                        child: const Text("OK"),
+                        onPressed: () {
+                          Navigator.pop(
+                              context, true); // Return true if confirmed
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ) ??
+              false; // Default to false if dialog is dismissed without selection
+        },
+        onDismissed: (direction) {
+          context.read<TaskInfoBloc>().add(RemoveTaskInfo(index: index));
+
+          var status =
+              context.read<VariableStateHandlerCubit<TaskStatusType>>().state!;
+          var loggedUser = context.read<LoggedUserInfoCubit>().state!;
+          context.read<TaskSaveBloc>().add(
+                TaskSave(
+                  userId: loggedUser.userId,
+                  taskStatus: status.value,
+                  taskId: data.tasksid ?? 0,
+                ),
+              );
+        },
+        background: Container(
+          decoration: BoxDecoration(
+            color: Colors.pink.shade800,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.save,
+                color: appTheme.white,
+              )
+            ],
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Colors.pink.shade800,
+                Colors.pink.shade800,
+              ],
+            ),
+          ),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: appTheme.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data.taskName ?? "",
+                  style: textTheme.bodyMedium!.copyWith(
+                    color: appTheme.primary,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Department",
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: appTheme.primary,
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        data.taskDept ?? "",
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: appTheme.primary,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Status",
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: appTheme.primary,
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        data.taskStatus ?? "",
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: appTheme.primary,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Start Date",
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: appTheme.primary,
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        DateTime.parse(data.taskStartDate ?? "")
+                            .toFormatedString("dd-MMM-yyyy"),
+                        style: textTheme.bodyMedium!.copyWith(
+                          color: appTheme.primary,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CommonDropdownButton<TaskStatusType>(
+                  value: context
+                      .watch<VariableStateHandlerCubit<TaskStatusType>>()
+                      .state,
+                  onChanged: (value) {
+                    if (value != null) {
+                      context
+                          .read<VariableStateHandlerCubit<TaskStatusType>>()
+                          .update(value);
+                    }
+                  },
+                  hintText: "Change Task Status",
+                  items: TaskStatusType.values,
+                ),
+              ],
+            ),
           ),
         ),
       ),
