b13ee024c667517aeb9507255047d4ba95a72f42
miraj98hossain <miraj09hossain@gmail.com>
2025-01-21 19:09:00 +0600

    Task Create ui modified

---
+++ lib/presentations/forms/om_forms/om_c_9_screen/model/child_task.dart
@@ -0,0 +1,11 @@
+class ChildTask {
+  final String taskName;
+  final String stDate;
+  final String enDate;
+
+  ChildTask({
+    required this.taskName,
+    required this.stDate,
+    required this.enDate,
+  });
+}
--- lib/presentations/forms/om_forms/om_c_9_screen/om_c_9_screen.dart
+++ lib/presentations/forms/om_forms/om_c_9_screen/om_c_9_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pran_rfl_erp/app_data/models/job_order_list_response.dart';
