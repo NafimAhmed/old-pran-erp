38942414daee80e7de7270ffb4f9a022a7e97f84
miraj98hossain <miraj09hossain@gmail.com>
2025-01-23 14:51:19 +0600

    Express Erp User List bloc move to global blocs

--- lib/presentations/forms/sys_admin_forms/sys_admin_c_5_screen/sys_admin_c_5_screen.dart
+++ lib/presentations/forms/sys_admin_forms/sys_admin_c_5_screen/sys_admin_c_5_screen.dart
@@ -11,7 +11,7 @@ import 'package:pran_rfl_erp/common_widgets/user_details_widget.dart';
 import 'package:pran_rfl_erp/core/theme/app_theme.dart';
 import 'package:pran_rfl_erp/global_blocs/cubit/logged_user_info_cubit.dart';
 import 'package:pran_rfl_erp/global_blocs/cubit/variable_state_handler_cubit.dart';
-import 'package:pran_rfl_erp/presentations/forms/sys_admin_forms/sys_admin_c_3_screen/bloc/qr_user_bloc.dart';
+import 'package:pran_rfl_erp/global_blocs/bloc/qr_user_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/sys_admin_forms/sys_admin_c_5_screen/bloc/org_access_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/sys_admin_forms/sys_admin_c_5_screen/bloc/org_bloc.dart';
 
