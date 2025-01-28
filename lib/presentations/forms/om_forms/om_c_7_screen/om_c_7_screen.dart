afceabaf6b9607c4ac23792ab671a56f07fd6cda
miraj98hossain <miraj09hossain@gmail.com>
2025-01-28 12:25:15 +0600

    task note add feature implemented

--- lib/presentations/forms/om_forms/om_c_7_screen/om_c_7_screen.dart
+++ lib/presentations/forms/om_forms/om_c_7_screen/om_c_7_screen.dart
@@ -5,16 +5,21 @@ import 'package:pran_rfl_erp/app_data/models/task_info_response.dart';
 import 'package:pran_rfl_erp/app_data/models/user_info_model.dart';
 import 'package:pran_rfl_erp/app_dependency/di_container.dart';
 import 'package:pran_rfl_erp/common_widgets/common_app_bar_widget.dart';
+import 'package:pran_rfl_erp/common_widgets/common_dialog_header.dart';
 import 'package:pran_rfl_erp/common_widgets/common_drop_down_menu_widget.dart';
+import 'package:pran_rfl_erp/common_widgets/common_text_field_widget.dart';
 import 'package:pran_rfl_erp/common_widgets/custom_drop_down_button_widget.dart';
 import 'package:pran_rfl_erp/common_widgets/custom_snackBar_widget.dart';
 import 'package:pran_rfl_erp/core/extentions/extentions.dart';
 import 'package:pran_rfl_erp/core/theme/app_theme.dart';
+import 'package:pran_rfl_erp/core/utils/app_modal.dart';
 import 'package:pran_rfl_erp/global_blocs/cubit/logged_user_info_cubit.dart';
 import 'package:pran_rfl_erp/global_blocs/cubit/variable_state_handler_cubit.dart';
+import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/add_task_note_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/Jo_list_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/task_Save_bloc.dart';
 import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/task_info_bloc.dart';
+import 'package:pran_rfl_erp/presentations/forms/om_forms/om_c_7_screen/bloc/task_note_list_bloc.dart';
 
 class OmC7Screen extends StatelessWidget {
   const OmC7Screen({super.key, required this.fromName});
@@ -282,8 +287,8 @@ class TaskWidgetContent extends StatelessWidget {
           context.read<VariableStateHandlerCubit<TaskStatusType>>().reset();
         },
         background: Container(
-          decoration: BoxDecoration(
-            color: Colors.pink.shade800,
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 106, 165, 66),
           ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -299,13 +304,13 @@ class TaskWidgetContent extends StatelessWidget {
           ),
         ),
         child: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.centerLeft,
               end: Alignment.centerRight,
               colors: [
-                Colors.pink.shade800,
-                Colors.pink.shade800,
+                Color.fromARGB(255, 106, 165, 66),
+                Color.fromARGB(255, 106, 165, 66),
               ],
             ),
           ),
@@ -414,45 +419,246 @@ class TaskWidgetContent extends StatelessWidget {
                 const SizedBox(
                   height: 5,
                 ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.37,
-                    child: CommonDropdownButton<TaskStatusType>(
-                      value: context
-                          .watch<VariableStateHandlerCubit<TaskStatusType>>()
-                          .state,
-                      onChanged: (value) {
-                        if (value != null) {
-                          context
-                              .read<VariableStateHandlerCubit<TaskStatusType>>()
-                              .update(value);
-                        }
-                      },
-                      hintText: "Change Status",
-                      items: TaskStatusType.values,
+                Row(
+                  children: [
+                    Expanded(
+                      child: CommonDropdownButton<TaskStatusType>(
+                        value: context
+                            .watch<VariableStateHandlerCubit<TaskStatusType>>()
+                            .state,
+                        onChanged: (value) {
+                          if (value != null) {
+                            context
+                                .read<
+                                    VariableStateHandlerCubit<TaskStatusType>>()
+                                .update(value);
+                          }
+                        },
+                        hintText: "Change Status",
+                        items: TaskStatusType.values,
+                      ),
                     ),
-                  ),
-                )
-                // Row(
-                //   children: [
-                //     Expanded(
-                //       child: ,
-                //     ),
-                //     // const SizedBox(
-                //     //   width: 5,
-                //     // ),
-                //     // IconButton.filledTonal(
-                //     //   onPressed: () {},
-                //     //   icon: Icon(
-                //     //     Icons.note_add,
-                //     //     color: appTheme.white,
-                //     //   ),
-                //     // ),
-                //   ],
-                // ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    IconButton.filledTonal(
+                      onPressed: () {
+                        var loggedUser =
+                            context.read<LoggedUserInfoCubit>().state!;
+                        AppModal.showCustomModal(
+                          context,
+                          content: NoteDialogWidget(
+                            taskInfo: data,
+                            userId: loggedUser.userId,
+                          ),
+                        );
+                      },
+                      icon: Icon(
+                        Icons.note_add,
+                        color: appTheme.white,
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NoteDialogWidget extends StatelessWidget {
+  const NoteDialogWidget({
+    super.key,
+    required this.taskInfo,
+    required this.userId,
+  });
+  final TaskInfo taskInfo;
+  final String userId;
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddTaskNoteBloc(getService()),
+        ),
+        BlocProvider(
+          create: (context) => TaskNoteListBloc(getService())
+            ..add(
+                TaskNoteListGet(userId: userId, taskId: taskInfo.tasksid ?? 0)),
+        ),
+      ],
+      child: NoteDialogContent(taskInfo: taskInfo, userId: userId),
+    );
+  }
+}
+
+class NoteDialogContent extends StatefulWidget {
+  const NoteDialogContent({
+    super.key,
+    required this.taskInfo,
+    required this.userId,
+  });
+  final TaskInfo taskInfo;
+  final String userId;
+  @override
+  State<NoteDialogContent> createState() => _NoteDialogContentState();
+}
+
+class _NoteDialogContentState extends State<NoteDialogContent> {
+  final GlobalKey<FormState> _fromKey = GlobalKey<FormState>();
+  late TextEditingController _noteController;
+  late FocusNode _noteFocusNode;
+  @override
+  void initState() {
+    _noteController = TextEditingController();
+    _noteFocusNode = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _noteController.dispose();
+    _noteFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AddTaskNoteBloc, AddTaskNoteState>(
+      listener: (context, state) {
+        if (state is AddTaskNoteSuccess) {
+          _noteController.clear();
+          context.read<TaskNoteListBloc>().add(
+                TaskNoteListGet(
+                  userId: widget.userId,
+                  taskId: widget.taskInfo.tasksid ?? 0,
+                ),
+              );
+        }
+      },
+      child: Container(
+        constraints: const BoxConstraints(
+          maxHeight: 400,
+        ),
+        padding: const EdgeInsets.all(5),
+        child: Form(
+          key: _fromKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CommonDialogHeader(
+                title: widget.taskInfo.taskName ?? "",
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 65,
+                child: CommonTextFieldWidget(
+                  focusNode: _noteFocusNode,
+                  controller: _noteController,
+                  labelText: "New Note",
+                  expands: true,
+                  maxLines: null,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Note can't be empty";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              BlocBuilder<AddTaskNoteBloc, AddTaskNoteState>(
+                builder: (context, state) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      if (_fromKey.currentState!.validate()) {
+                        context.read<AddTaskNoteBloc>().add(
+                              AddTaskNote(
+                                userId: widget.userId,
+                                taskNote: _noteController.text,
+                                taskId: widget.taskInfo.tasksid ?? 0,
+                              ),
+                            );
+                      }
+                    },
+                    child: Text(
+                      state is AddTaskNoteLoading ? "Adding..." : "Add Note",
+                      style: textTheme.bodyMedium!.copyWith(
+                        color: appTheme.white,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Divider(
+                color: appTheme.dividerColor,
+                height: 2,
+                indent: 5,
+                endIndent: 5,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Expanded(
+                child: BlocBuilder<TaskNoteListBloc, TaskNoteListState>(
+                  builder: (context, state) {
+                    if (state is TaskNoteListLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    if (state is TaskNoteListSuccess) {
+                      return ListView.separated(
+                        itemBuilder: (context, index) {
+                          var data = state.taskNoteList[index];
+                          return Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: appTheme.primary.withOpacity(0.3),
+                                borderRadius: BorderRadius.circular(5)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  data.noteContent ?? "",
+                                  style: textTheme.bodyMedium!.copyWith(
+                                    color: appTheme.primary,
+                                  ),
+                                ),
+                                Text(
+                                  "Created By: ${data.createdBy}",
+                                  style: textTheme.bodyMedium!.copyWith(
+                                    color: appTheme.primary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return const SizedBox(
+                            height: 5,
+                          );
+                        },
+                        itemCount: state.taskNoteList.length,
+                      );
+                    }
+                    return Container();
+                  },
+                ),
+              )
+            ],
           ),
         ),
       ),
