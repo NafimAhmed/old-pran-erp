80011ca64b499dffa3a167074c42fb51eb9b4d3a
Miraj <miraj09hossain@gmail.com>
2025-02-19 09:30:38 +0600

    un used imports removed

--- lib/presentations/user_profile/user_profile_screen/user_profile_screen.dart
+++ lib/presentations/user_profile/user_profile_screen/user_profile_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pran_rfl_erp/app_data/models/user_info_model.dart';
 import 'package:pran_rfl_erp/app_dependency/di_container.dart';
-import 'package:pran_rfl_erp/common_widgets/common_app_bar_widget.dart';
 import 'package:pran_rfl_erp/common_widgets/common_dialog_header.dart';
 import 'package:pran_rfl_erp/common_widgets/common_text_field_widget.dart';
 import 'package:pran_rfl_erp/common_widgets/custom_snackBar_widget.dart';
