fad7fcc2522de1f496755a71aebc5e56e0f48589
Miraj <miraj09hossain@gmail.com>
2025-07-09 12:07:51 +0600

    feat: implement GrnItemQrCubit for managing GRN item QR data and state

---
+++ lib/presentations/forms/inv_forms/inv_c_10_screen/cubit/grn_item_qr_cubit.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pran_rfl_erp/app_data/models/grn_qr_list_response.dart';
+
+@immutable
+sealed class GrnItemQrState {}
+
+final class GrnItemQrDataLoaded extends GrnItemQrState {
+  final GrnQr grnQr;
+
+  GrnItemQrDataLoaded({required this.grnQr});
+}
+
+final class GrnItemQrDataError extends GrnItemQrState {
+  final Object error;
+
+  GrnItemQrDataError({required this.error});
+}
+
+final class GrnItemQrInitial extends GrnItemQrState {}
+
+class GrnItemQrCubit extends Cubit<GrnItemQrState> {
+  GrnItemQrCubit() : super(GrnItemQrInitial());
+  void setItemData({required String grnItemQrData}) {
+    try {
+      var list = grnItemQrData.split("\n");
+      GrnQr grnQr = GrnQr();
+      grnQr = grnQr.copyWith(
+        trnid: list[0],
+        orgId: int.parse(list[1]),
+        qty: num.parse(list[2]),
+        inventoryItemId: int.parse(list[3]),
+        itemCode: list[4].split("-")[0],
+        itemName: list[4].split("-")[1],
+        subInv: list[5],
+        locatorDesc: list[6],
+      );
+
+      emit(GrnItemQrDataLoaded(grnQr: grnQr));
+    } catch (error) {
+      emit(GrnItemQrDataError(error: error));
+    }
+  }
+
+  void resetItemData() {
+    emit(GrnItemQrInitial());
+  }
+}
