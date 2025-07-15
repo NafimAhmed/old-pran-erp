eb867e78bacf0bf59b81432276b02388f8ca0ff4
Miraj <miraj09hossain@gmail.com>
2025-07-15 18:01:39 +0600

    Completer error fixed in qr scanner builder

--- lib/core/utils/healper_functions.dart
+++ lib/core/utils/healper_functions.dart
@@ -5,7 +5,9 @@ import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:pran_rfl_erp/common_widgets/qr_scanner_widget.dart';
 
 Future<String> buildScanner(
-    BuildContext context, MobileScannerController? controller) async {
+  BuildContext context,
+  MobileScannerController? controller,
+) async {
   // Use a completer to wait for the scanned result
   final completer = Completer<String>();
 
@@ -15,14 +17,8 @@ Future<String> buildScanner(
     builder: (context) {
       return Dialog(
         alignment: Alignment.center,
-        insetPadding: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-            5.0,
-          ),
-        ),
+        insetPadding: const EdgeInsets.symmetric(horizontal: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
         child: Container(
           height: MediaQuery.of(context).size.height * 0.35,
           width: MediaQuery.of(context).size.width * 0.35,
@@ -30,15 +26,16 @@ Future<String> buildScanner(
           child: QrScannerWidget(
             controller: controller,
             onDetect: (barcodes) {
-              final String detectedData = barcodes.barcodes.isNotEmpty
-                  ? barcodes.barcodes.first.rawValue ?? 'No data found'
-                  : 'No data found';
+              if (!completer.isCompleted) {
+                final String detectedData = barcodes.barcodes.isNotEmpty
+                    ? barcodes.barcodes.first.rawValue ?? 'No data found'
+                    : 'No data found';
 
-              // Complete the completer with the detected data
-              completer.complete(detectedData);
+                completer.complete(detectedData);
 
-              // Close the dialog
-              Navigator.of(context).pop();
+                // Close the dialog
+                Navigator.of(context).pop();
+              }
             },
           ),
         ),
